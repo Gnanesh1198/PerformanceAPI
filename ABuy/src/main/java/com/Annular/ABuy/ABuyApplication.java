8977b6662c283e4bf7b2b8468b72ca876ed954cb
Gnanesh1198 <gnaneshwaran.rajendran@gmail.com>
2023-01-06 14:28:11 +0530

    aa

---
+++ ABuy/src/main/java/com/Annular/ABuy/ABuyApplication.java
@@ -0,0 +1,13 @@
+package com.Annular.ABuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ABuyApplication.class, args);
+	}
+
+}
---
+++ ABuy/src/test/java/com/Annular/ABuy/ABuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Annular.ABuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ABuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
